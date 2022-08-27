882c21229f846970cb03c3336d72b828380b2a1d
Mikhail Yuzhakov <miha.yuzhakov@gmail.com>
2022-08-27 23:50:12 +0300

    добавлен драйвер bluetooth

--- L3Driver.h
+++ L3Driver.h
@@ -5,14 +5,15 @@
 #pragma once
 
 #include <SoftwareSerial.h>
+#include <BluetoothSerial.h>
 
 class L3Driver
 {
 	public:
 		virtual void Init(){ }
 		virtual uint8_t ReadAvailable(){ }
-		virtual byte ReadByte(){ }
-		virtual void SendByte(byte data){ }
+		virtual uint8_t ReadByte(){ }
+		virtual void SendByte(uint8_t data){ }
 };
 
 /*
@@ -35,12 +36,12 @@ class L3DriverRAW : public L3Driver
 			return Serial.available();
 		}
 		
-		byte ReadByte() override
+		uint8_t ReadByte() override
 		{
 			return Serial.read();
 		}
 		
-		void SendByte(byte data) override
+		void SendByte(uint8_t data) override
 		{
 			Serial.write(data);
 			
@@ -53,9 +54,37 @@ class L3DriverUART : public L3Driver
 	
 };
 
+/*
+	Драйвер работы с bluetooth.
+*/
 class L3DriverBluetooth : public L3Driver
 {
+	public:
+		void Init() override
+		{
+			SerialBT.begin("StarPixel"); // указываем имя устройства
+			return;
+		}
+		
+		uint8_t ReadAvailable() override
+		{
+			return SerialBT.available();
+		}
+		
+		uint8_t ReadByte() override
+		{
+			return SerialBT.read();
+		}
+		
+		void SendByte(uint8_t data) override
+		{
+			SerialBT.write(data);
+			
+			return;
+		}
 	
+	private:
+		BluetoothSerial SerialBT;		
 };
 
 
@@ -64,38 +93,38 @@ class L3DriverBluetooth : public L3Driver
 	Использует пины: RX:2, RX:3.
 	Скорость: 19200.
 */
-class L3DriverSoftSerial : public L3Driver
-{
-	public:
-		L3DriverSoftSerial() : mySerial(2, 3)
-		{
-			return;
-		}
+// class L3DriverSoftSerial : public L3Driver
+// {
+// 	public:
+// 		L3DriverSoftSerial() : mySerial(2, 3)
+// 		{
+// 			return;
+// 		}
 		
-		void Init() override
-		{
-			mySerial.begin(19200);
+// 		void Init() override
+// 		{
+// 			mySerial.begin(19200);
 			
-			return;
-		}
+// 			return;
+// 		}
 		
-		uint8_t ReadAvailable() override
-		{
-			return mySerial.available();
-		}
+// 		uint8_t ReadAvailable() override
+// 		{
+// 			return mySerial.available();
+// 		}
 		
-		byte ReadByte() override
-		{
-			return mySerial.read();
-		}
+// 		uint8_t ReadByte() override
+// 		{
+// 			return mySerial.read();
+// 		}
 		
-		void SendByte(byte data) override
-		{
-			mySerial.write(data);
+// 		void SendByte(uint8_t data) override
+// 		{
+// 			mySerial.write(data);
 			
-			return;
-		}
+// 			return;
+// 		}
 	
-	private:
-		SoftwareSerial mySerial;
-};
+// 	private:
+// 		SoftwareSerial mySerial;
+// };

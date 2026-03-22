e09918d4b2996f28e0d0de862523a49cf2e3e6cc
Dragon-Knight <dubki4132@mail.ru>
2026-03-22 05:02:40 +0300

    Init `CANCore.h`

---
+++ include/CANCore.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <inttypes.h>
+#include <DrakePinD.hpp>
+
+namespace CANCore
+{
+	DrakePinD rs({0, GPIO_NUM_6}, DrakePin::OutputOpenDrain, DrakePin::High);
+
+
+	inline void Setup()
+	{
+		rs.Init();
+
+
+		rs.Write(DrakePin::Low);
+		
+		
+		return;
+	}
+	
+	inline void Loop(uint32_t &time)
+	{
+		
+		
+		time = millis();
+		return;
+	}
+}
